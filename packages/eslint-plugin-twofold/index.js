1d6bb37a46aed19ec2e08a3d89f91713b501e901
Ryan Toronto <ryanto@gmail.com>
2024-10-11 23:20:49 -0400

    upgrade eslint

--- packages/eslint-plugin-twofold/index.js
+++ packages/eslint-plugin-twofold/index.js
@@ -1,20 +1,24 @@
+let eslint = require("@eslint/js");
+let tseslint = require("typescript-eslint");
 let eslintPluginReact = require("eslint-plugin-react");
 let eslintPluginReactHooks = require("eslint-plugin-react-hooks");
 let globals = require("globals");
-let fs = require('fs');
-let path = require('path');
+let fs = require("fs");
+let path = require("path");
 
-let packageJsonPath = path.join(__dirname, 'package.json');
-let packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+let packageJsonPath = path.join(__dirname, "package.json");
+let packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 let version = packageJson.version;
 
 let plugin = {
   meta: {
     name: "eslint-plugin-twofold",
-    version
+    version,
   },
   configs: {
     recommended: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
       {
         ignores: [".twofold/"],
       },
@@ -37,12 +41,13 @@ let plugin = {
         },
         rules: {
           ...eslintPluginReactHooks.configs.recommended.rules,
+          "no-unused-vars": "off",
         },
-      }
-    ]
+      },
+    ],
   },
   rules: {},
-  processors: {}
+  processors: {},
 };
 
 module.exports = plugin;
