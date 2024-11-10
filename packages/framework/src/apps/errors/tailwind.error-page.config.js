05f16b9278106b84ff958633174990abf62ec26b
Ryan Toronto <ryanto@gmail.com>
2024-11-09 19:40:19 -0500

    dont rely on __dirname

--- packages/framework/src/apps/errors/tailwind.error-page.config.js
+++ packages/framework/src/apps/errors/tailwind.error-page.config.js
@@ -1,3 +1,9 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+let __filename = fileURLToPath(import.meta.url);
+let __dirname = dirname(__filename);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [`${__dirname}/**/*.{ts,tsx}`],
