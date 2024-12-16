a3eadd7f4d96e011182b279c2e1f1a58099f3611
Ryan Toronto <ryanto@gmail.com>
2024-12-16 17:31:40 -0500

    website updates

--- sites/website/tailwind.config.js
+++ sites/website/tailwind.config.js
@@ -1,8 +1,10 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };
