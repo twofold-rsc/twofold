d3b4e9fd6e66e8aab1e7a3b77bd84a54d5c8b980
Ryan Toronto <ryanto@gmail.com>
2025-11-16 12:08:22 -0500

    fix typo

--- packages/create-twofold-app/template/app/pages/index.page.tsx
+++ packages/create-twofold-app/template/app/pages/index.page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
           Welcome to Twofold
         </h1>
         <p className="mt-4 max-w-prose text-lg">
-          Get started with React Server Component by editing{" "}
+          Get started with React Server Components by editing{" "}
           <span className="rounded bg-gray-100 px-1 py-0.5 font-mono text-sm">
             app/pages/index.page.tsx
           </span>
